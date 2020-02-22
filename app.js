8861395213eaecb35f19a719d199f217597e704c
Pablo Cianes <cianespablo@gmail.com>
2020-02-22 12:29:41 +0100

    ✨ Example of implementation with simple form

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-G$( 'Pablo', 'Cianes')
-    .log()
-    .greet()
-    .greet(true)
-    .setLang('es')
-    .greet()
-    .greet(true)
-    .HTMLGreeting('#greeting', true );
+$('#login').click( function(){
+
+    const language = $('#lang').val(),
+        firstName = $('#firstName').val(),
+        lastName = $('#lastName').val();
+
+    /* hide the login on the screen after click on the login button */
+    $('#logindiv').hide();
+
+    /* 
+        Gets a new object and use our chainable methods
+        ( the architecture allows us to not have to use the 'new' kewyword here ) 
+    */
+    G$( firstName, lastName )
+        .setLang( language )
+        .HTMLGreeting( '#greeting', true )
+        .log();
+});
