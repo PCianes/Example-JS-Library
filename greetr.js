ce21371525cdd00e2d4150761dc6b780fe82ade2
Pablo Cianes <cianespablo@gmail.com>
2020-02-22 12:07:41 +0100

    📒 Add comments

--- greetr.js
+++ greetr.js
@@ -1,9 +1,11 @@
 ;( function( global, $ ){
 
+    /* Init "new" main object with helper method: init */
     const Greetr = function( firstName, lastName, language ){
         return new Greetr.init( firstName, lastName, language );
     }
 
+    /* Hidden within the scope of the IIFE and never directly accesible */
     const supportedLangs = ['en', 'es'];
     const greetings = {
         en: 'Hello',
@@ -18,18 +20,24 @@
         es: 'Inició sesión',
     };
 
+    /* 
+        Prototype holds methods (to save memory space) 
+        'this' refers to the calling object at execution time
+    */
     Greetr.prototype = {
 
+        /* Chainable methods return their own containing object */
         greet: function(formal) {
+            /* In case of undefined or null it will be coerced to 'false' */
             const msg = formal ? this.formalGreeting() : this.greeting();
 
             console.log(msg);
             
-            //makes the method chainable
-            // 'this' refers to the calling object at execution time
+            /* Makes the method chainable */
             return this;
         },
 
+        /* Retrieve messages from object by referring to properties using [] syntax */
         greeting: function() {
             return `${greetings[ this.language ]} ${this.firstName}!`;
         },
@@ -43,13 +51,19 @@
         },
 
         setLang: function( language ){
+            /* Set the language */
             this.language = language;
 
             this.validate();
-
+            
+            /* Makes the method chainable */
             return this;
         },
 
+        /* 
+            Check that is a valid language
+            References the externally inaccessible 'supportedLangs' within the closure
+        */
         validate: function(){
             if ( -1 === supportedLangs.indexOf( this.language ) ) {
                 throw "Invalid language";
@@ -59,6 +73,7 @@
         log: function(){
             console.log(`${logMessages[ this.language ]}: ${this.fullName()}`);
             
+            /* Makes the method chainable */
             return this;
         },
 
@@ -70,12 +85,18 @@
                 throw 'Missing jQuery selector';
             }
             
+            /* Inject the message in the chosen place in the DOM with jQuery */
             $( selector ).html( formal ? this.formalGreeting() : this.greeting() );
 
+            /* Makes the method chainable */
             return this;
         }
     };
 
+    /* 
+        Main object is created here, allowing us to 'new' an object without calling 'new'
+        Trick borrowed from jQuery so we don't have to use the 'new' keyword 
+    */
     Greetr.init = function( firstName, lastName, language ){
 
         const self = this;
@@ -83,10 +104,13 @@
         self.lastName = lastName || '';
         self.language = language || 'en';
 
+        self.validate();
     }
 
+    /* Allow to add methods to main object but using Greetr as more readable for code */
     Greetr.init.prototype = Greetr.prototype;
 
+    /* Attach our Greetr to the global object, and provide a shorthand '$G' for ease our poor fingers */
     global.Greetr = global.G$ = Greetr;
 
 }( window, jQuery ) );
